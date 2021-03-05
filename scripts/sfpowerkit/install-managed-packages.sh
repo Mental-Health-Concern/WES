3c4526f7498a38316954552a85a95bae1412c04c
Lawrence Newcombe <32834730+LawrenceLoz@users.noreply.github.com>
2021-03-05 16:05:33 +0700

    Install package dependencies with sfpowerkit

--- scripts/sfpowerkit/install-managed-packages.sh
+++ scripts/sfpowerkit/install-managed-packages.sh
@@ -1,2 +1,1 @@
-echo 'y' | sfdx force:package:install --package "Rollup Helper@15.11.0.10"
-echo 'y' | sfdx force:package:install --package "Nintex DocGen@16.5.0.2"
+sfdx sfpowerkit:package:dependencies:install -u $1 -v $2 -r
