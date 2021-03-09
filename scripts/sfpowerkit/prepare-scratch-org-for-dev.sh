391c75a7fb9a36d835e50410d59252b973dbfbd0
Lawrence Newcombe <32834730+LawrenceLoz@users.noreply.github.com>
2021-03-09 19:25:53 +0700

    Removing call from scripts

--- scripts/sfpowerkit/prepare-scratch-org-for-dev.sh
+++ scripts/sfpowerkit/prepare-scratch-org-for-dev.sh
@@ -12,16 +12,16 @@ devhubusername=$2
 
 # Install all managed packages identified as dependencies in the sfdx-project.json
 echo Installing managed package dependencies for scratch org $username...
-call sfdx sfpowerkit:package:dependencies:install -u $username -v $devhubusername -r
+sfdx sfpowerkit:package:dependencies:install -u $username -v $devhubusername -r
 
 # Push source metadata
 echo Pushing metadata...
-call sfdx force:source:push -u $username -r
+sfdx force:source:push -u $username -r
 
 # Assign all permission sets and permission set groups specified by name in config/wes-permission-set-groups-for-admin.txt
 # Remember to add new ones into the file to make sure they're assigned :)
 echo Assigning permission sets to default user...
 while read permSetName; do
     echo Assigning $permSetName...
-    call sfdx force:user:permset:assign -u $username --permsetname $permSetName
+    sfdx force:user:permset:assign -u $username --permsetname $permSetName
 done <config/wes-permission-set-groups-for-admin.txt
